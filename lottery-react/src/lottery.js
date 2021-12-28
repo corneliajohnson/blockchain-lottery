48f29937a098e620adbaf3b4e0e74e5eed526305
corneliajohnson <corneliaj91@gmail.com>
2021-12-28 16:48:40 -0600

    add the manager information to the dom

---
+++ lottery-react/src/lottery.js
@@ -0,0 +1,62 @@
+//This file holds contract information from deploy.js
+
+import web3 from "./web3";
+
+const address = "0xC97f99b37Aa76a3f75F5Bd03B18faB5282243459";
+
+const abi = [
+  {
+    constant: true,
+    inputs: [],
+    name: "manager",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "pickWinner",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "getPlayers",
+    outputs: [{ name: "", type: "address[]" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "enter",
+    outputs: [],
+    payable: true,
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [{ name: "", type: "uint256" }],
+    name: "players",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+];
+
+//create local instance of contract
+export default new web3.eth.Contract(abi, address);
